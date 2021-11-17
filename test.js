e080815904ce6f95b8a61f4108cb61dc784dd24d
lithiumflowers <71810641+lithiumflowers@users.noreply.github.com>
2021-11-17 13:49:56 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,46 +1,53 @@
 var timer = null,
-	timerInv = null,
 	index = 0,
-	img_count = 5,
+	img_count = 4,
 	locatArr = [0, -720, -1440, -2160,-2880];
 var box = document.getElementsByClassName('box')[0],
 	dot = document.getElementsByClassName('dot')[0];
 
 /**
-*轮播图片展示函数(最新，利用CSS3)，根据索引值到达相应地方
+*跳转到某张图片
+*@function moveto
+*@param location 索引值，具体位置存储在locatArr数组中
+*@return none
+*/
+function moveto(location) {
+	box.style.left = locatArr[location] + 'px';
+}
+
+/**
+*图片展示函数，根据索引值到达相应地方
 *@param none
 *@return none
 */
 function disPlay () {
-	let initLeft = parseInt(window.getComputedStyle(box).left);
 	if(index == img_count - 1){
-		moveTo(0);
+		moveto(0);
 		index = 0;
 	}else{
-		moveTo(++index);
+		moveto(++index);
 	}
 }
 
 /**
 *上一张/下一章图片
-*Function lastPlay/nextPlay
 *@param none
 *@return none
 */
 function lastPlay () {
 	if(index == 0){
-		moveTo(4);
-		index = 4;
+		moveto(3);
+		index = 3;
 	}else{
-		moveTo(--index);
+		moveto(--index);
 	}
 }
 function nextPlay () {
-	if(index == 4){
-		moveTo(0);
+	if(index == 3){
+		moveto(0);
 		index = 0;
 	}else{
-		moveTo(++index);
+		moveto(++index);
 	}
 }
 /**
@@ -50,9 +57,9 @@ function nextPlay () {
 *@return none
 */
 function bindEvent() {
-	let li = dot.getElementsByTagName('li');
-	let next = document.getElementsByClassName('next')[0];
-	let last = document.getElementsByClassName('last')[0];
+	var li = dot.getElementsByTagName('li');
+	var next = document.getElementsByClassName('next')[0];
+	var last = document.getElementsByClassName('last')[0];
 	next.addEventListener('click',function () {
 		clearInterval(timer);
 		nextPlay();
@@ -60,45 +67,33 @@ function bindEvent() {
 	});
 	last.addEventListener('click',function() {
 		clearInterval(timer);
-		lastPlay();		
+		lastPlay();	
 		timer = setInterval(disPlay,2000);
 	});
-	for(var i = 0; i < 5; i++){
+	for(var i = 0; i < 4; i++){
 		(function (j) {
 			li[j].addEventListener('click',function () {
 				clearInterval(timer);
 				index = j;
-				moveTo(index);
+				moveto(index);
 				timer = setInterval(disPlay,2000);
 			})
 		}(i))
 		
 	}
 }
-/**
-*跳转到某张图片
-*@function moveTo
-*@param location 索引值，具体位置存储在locatArr数组中
-*@return none
-*/
-function moveTo(location) {
-	var target_position = parseInt(window.getComputedStyle(box).left) - locatArr[location];
-	box.style.left = locatArr[location] + 'px';
-}
-function start() {
 
+function start() {
 	timer = setInterval(disPlay,2000);
-	var timerDot = setInterval(function () {
-		var dots = document.getElementsByClassName('dot')[0];
-		var singledot = dots.getElementsByTagName('li');
-		for(var i = 0; i < 5; i++){
+	let timerDot = setInterval(function () {
+		let dots = document.getElementsByClassName('dot')[0];
+		let singledot = dots.getElementsByTagName('li');
+		for(var i = 0; i < 4; i++){
 			singledot[i].className = '';
 		}
 		singledot[index].className = 'act';
-
 	},20)
 	bindEvent();
 }
 
 start();
-	
